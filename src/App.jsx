addeb2aa81cc0e1537f468e0ae8db69c26b17adf
Farzan <farzanhasan85@gmail.com>
2024-07-08 23:50:11 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
-        <Sidebar />
+        <Sidebar/>
         <Display />
       </div>
       <Player />
